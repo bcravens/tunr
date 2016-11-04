9be837702ba1e1c41a413d1136f441c4024cc555
Bradley Cravens <bradleycravens@gmail.com>
2016-11-04 12:15:51 -0400

    inital commit

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,9 +1,40 @@
 class ArtistsController < ApplicationController
+  before_action :get_artist, only: [:show, :destroy, :edit, :update]
   def index
     @artists = Artist.all
   end
 
   def show
+  end
+
+  def new
+    @artist = Artist.new
+  end
+
+  def create
+    @artist = Artist.create!(artist_params)
+    redirect_to artists_path
+  end
+
+  def destroy
+    @artist.destroy
+    redirect_to artists_path
+  end
+
+  def edit
+  end
+
+  def update
+    @artist.update(artist_params)
+    redirect_to artists_path
+  end
+
+  private
+  def artist_params
+    params.require(:artist).permit(:name, :photo_url, :nationality)
+  end
+
+  def get_artist
     @artist = Artist.find(params[:id])
   end
 
